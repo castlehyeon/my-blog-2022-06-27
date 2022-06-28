36340e3a7e657433e47b67078dbb6fa0266a4cba
castlehyeon <qnftktls118@naver.com>
2022-06-28 11:27:26 +0900

    HEAD component 추가

--- src/pages/test-space.js
+++ src/pages/test-space.js
@@ -1,21 +1,16 @@
-import { Link } from "gatsby";
 import React from "react";
+import Head from "../components/head";
 
 function TestSpacePage() {
   return (
     <main>
       <title>테스트 스페이스</title>
 
-      <div style={{ display: "flex", gap: 10 }}>
-        <Link to="/">홈</Link>
-        <Link to="/about">어바웃</Link>
-        <Link to="/test">테스트</Link>
-        <Link to="/test-space">테스트-스페이스</Link>
-      </div>
+      <Head />
 
       <h1>테스트 스페이스 페이지 입니다.</h1>
     </main>
   );
 }
 
-export default TestSpacePage;//밖에서 페이지가 들어올 수 있게 함.
+export default TestSpacePage;
