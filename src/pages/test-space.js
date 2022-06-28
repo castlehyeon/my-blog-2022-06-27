a5fe379f64c9ca12b63bca592f254f184512bc78
castlehyeon <qnftktls118@naver.com>
2022-06-28 10:48:21 +0900

    테스트 페이지 추가

---
+++ src/pages/test-space.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function TestSpacePage() {
+    return <main>
+        <title>테스트 스페이스</title>
+        <h1>테스트 스페이스입니다.</h1>
+        </main>
+}
+
+export default TestSpacePage; //밖에서 페이지가 들어올 수 있게 함.
---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function AboutPage() {
+    return <main>
+        <title>자기소개</title>
+        <h1>자기소개 페이지입니다.</h1>
+        </main>
+}
+
+export default AboutPage; //밖에서 페이지가 들어올 수 있게 함.
